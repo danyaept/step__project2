477f6fadf1c18a3bee5cce9e61113da608a7738e
ОН-з21 Столяров Данііл Максимович <iamstolyarovdanya@gmail.com>
2023-10-01 18:35:37 +0300

    burger options

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,29 @@
 const burger = document.querySelector(`.burger_btn`);
-console.log(burger);
-// burger.addEventListener( `click`,(e)=>{
-// alert(`NIKITA HUI SOSI`)
-// })
+const ulNav = document.querySelector(`.header__navbar-list `);
+const burgerImg1 = document.querySelector(`.burger_btn__img1`);
+const burgerImg2 = document.querySelector(`.burger_btn__img2`);
+const burgerImg3 = document.querySelector(`.burger_btn__img3`)
+console.log(ulNav);
+console.log(burgerImg1);
+console.log(burgerImg2);
+console.log(burgerImg3);
+burger.addEventListener( `click`,(e)=>{
+ulNav.classList.toggle(`img_none`);
+if(!ulNav.classList.contains(`img_none`)){
+  burgerImg1.setAttribute(`src` , `images/burger_second_svg.svg`);
+  burgerImg2.setAttribute(`src` , `images/icon-exit.svg`);
+  burgerImg3.classList.add(`img_none`);
+  burgerImg1.style.top = `3px`;
+  burgerImg1.style.left = `5px`;
+  burgerImg2.style.top = `3px`;
+  burgerImg2.style.left = `5px`;
+}else{
+    burgerImg1.setAttribute(`src` , `images/burger_sm_svg.svg`);
+  burgerImg2.setAttribute(`src` , `images/icon-burger_menu.svg`);
+  burgerImg3.classList.remove(`img_none`);
+  burgerImg1.style.top = `8px`;
+  burgerImg1.style.left = `2px`;
+  burgerImg2.style.top = `18px`;
+  burgerImg2.style.left = `2px`;
+}
+})
